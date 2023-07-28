eebfc05b8b22dd6e8853e308a372e1581b73bc8e
Savenok <romasav2017@gmail.com>
2023-07-28 12:24:12 +0300

    sem4

--- SEM4/ez2.py
+++ SEM4/ez2.py
@@ -1,1 +1,21 @@
-# Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь, где ключ — значение переданного аргумента, а значение — имя аргумента. Если ключ не хешируем, используйте его строковое представление. Пример: rev_kwargs(res=1, reverse=[1, 2, 3]) -> {1: 'res', '[1, 2, 3]': 'reverse'}
+# Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь,
+# где ключ — значение переданного аргумента, а значение — имя аргумента. Если ключ не хешируем,
+# используйте его строковое представление.
+# Пример: rev_kwargs(res=1, reverse=[1, 2, 3]) -> {1: 'res', '[1, 2, 3]': 'reverse'}
+
+def rev_kwargs(**data):  # -> dict{*data : *data} - >dict{}:
+    res_dict = {}
+    for k, v in data.items():
+        res_dict[v] = k
+        print(res_dict)
+    return res_dict
+
+# Можно ли сделать так?
+# def rev_kwargs(**data):  # -> dict{*data : *data} - >dict{}:
+#     res_dict = {v: k for k, v in data.items()}
+#     return res_dict
+
+
+print(rev_kwargs(res=1, rev=[1, 2, 3]))
+print(rev_kwargs(rev=[1, 2, 3], revr=0))
+print(rev_kwargs(N=None, string=str(1)))
--- SEM4/ez3.py
+++ SEM4/ez3.py
@@ -9,3 +9,85 @@
 # # операцией, даже ошибочной
 # # ✔ Любое действие выводит сумму денег
 # Возьмите задачу о банкомате из семинара 2. Разбейте её на отдельные операции — функции. Дополнительно сохраняйте все операции поступления и снятия средств в список.
+
+def main():
+    log_list = []  # История операции
+    start_sum = 0  # Стартовая сумма
+    count_operation = 0
+    while True:
+        menu(start_sum, log_list, count_operation)
+
+
+def menu(start_sum: int, log_list: list[int], count_operation: int):
+    if count_operation == 3:
+        start_sum += (start_sum // 100 * 3)
+    var = int(input("Выберите действие:\n1 - Пополнить баланс\n2 - Снять деньги\n0 - Exit\n : "))
+    if var == 1:
+        count_operation += 1
+        get_money(start_sum, log_list, count_operation)
+    if var == 2:
+        count_operation += 1
+        put_money(start_sum, log_list, count_operation)
+    if var == 0:
+        exit()
+    if var == 100:
+        print(log_list)
+        menu(start_sum, log_list, count_operation)
+
+
+def get_money(start_sum: int, log_list: list[int], count_operation: int):
+    max_balace = 5000000  # Максимальная сумма баланса
+    summ = int(input("Введите сумму, на которую хотите пополнить баланс: "))
+    percent = summ // 100 * 10
+    if summ % 50 == 0:
+        if start_sum < max_balace:
+            start_sum += summ
+            print(f"Баланс: {start_sum}")
+            log_list.append(f"+ {summ}, balance = {start_sum}")
+            menu(start_sum, log_list, count_operation)
+        else:
+            print("ТАк как баланс больше 5млн коммисия становить 10%")
+            start_sum += (summ - percent)
+            print(f"Баланс: {start_sum}")
+            log_list.append(f"+ {summ}, balance = {start_sum}")
+            menu(start_sum, log_list, count_operation)
+    else:
+        print("Повторите операцию")
+        get_money(start_sum, log_list, count_operation)
+
+
+def put_money(start_sum: int, log_list: list[int], count_operation: int):
+    if start_sum == 0:
+        print(f"Операция не возможна, баланс равен {start_sum}")
+    else:
+        min_l = 30
+        max_l = 600
+        wealth_tax = 10  # Налог на богатство (%)
+        panal = 1.5  # Коммисия за снятие (%)
+        summ = int(input("Введите сумму, которую хотите снять: "))
+        percent = 0
+        if 30 <= summ // 100 * 1.5 <= 600:
+            percent = summ // 100 * 1.5
+        if percent == 600:
+            percent == 600
+        else:
+            percent = 30
+
+        if summ % 50 == 0:
+            if summ < start_sum:
+                start_sum = start_sum - summ - percent
+                print(f"Баланс: {start_sum}")
+                log_list.append(f"-{summ}, balance= {start_sum}")
+                menu(start_sum, log_list, count_operation)
+            else:
+                print(f"На балансе не хватает средст для снятия, повторите попытку")
+                print(f"Баланс: {start_sum}")
+                put_money(start_sum, log_list, count_operation)
+
+        else:
+            print("Повторите операцию")
+            put_money(start_sum, log_list, count_operation)
+
+
+main()
+
